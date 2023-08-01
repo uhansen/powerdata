3aa8ca91f3e61792905ff5a190a13cb4809f1075
Uffe Hansen <uffe.hansen@gmail.com>
2023-08-01 19:05:45 +0200

    Added API - help

--- add-powerdata/src/lib.rs
+++ add-powerdata/src/lib.rs
@@ -8,8 +8,12 @@ use spin_sdk::{
 #[http_component]
 fn handle_add_powerdata(req: Request) -> Result<Response> {
     println!("{:?}", req.headers());
+    
     Ok(http::Response::builder()
         .status(200)
         .header("foo", "bar")
-        .body(Some("Hello, Fermyon".into()))?)
+        .body(Some("Powerdata added".into()))?)
 }
+
+//https://github.com/fermyon/code-things/commit/f40d033a25729baed8b8351ed2e28f37688dcfe3#diff-764055ca37d0f6c66c755b47c85025b44334699d510c23653f33d802e826a23f
+// https://www.fermyon.com/blog/building-a-social-app-with-spin-1
