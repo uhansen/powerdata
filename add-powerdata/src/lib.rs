ea02207456ecd4ea16c54320e5298c0671fbb244
Uffe Hansen <uffe.hansen@gmail.com>
2023-08-02 09:24:19 +0200

    config help link added

--- add-powerdata/src/lib.rs
+++ add-powerdata/src/lib.rs
@@ -17,3 +17,4 @@ fn handle_add_powerdata(req: Request) -> Result<Response> {
 
 //https://github.com/fermyon/code-things/commit/f40d033a25729baed8b8351ed2e28f37688dcfe3#diff-764055ca37d0f6c66c755b47c85025b44334699d510c23653f33d802e826a23f
 // https://www.fermyon.com/blog/building-a-social-app-with-spin-1
+// https://www.thorsten-hans.com/master-configuration-data-in-fermyon-spin/
